414ae4d7982a9ec482c15e69e0bb76a5c8f43d3f
upeh <n.latifahulfah31@gmail.com>
2017-03-15 13:46:40 +0700

    apa ini lupa

---
+++ VZ03/file_reader.cpp
@@ -0,0 +1,199 @@
+#include "file_reader.h"
+
+/**
+* @brief Constructor FileReader
+* @param namafile Namafile yang akan dibaca
+*/
+FileReader::FileReader(string namafile) {
+	filename = namafile;
+
+	file.open(filename);
+}
+
+/**
+* @brief Destruktor
+*/
+FileReader::~FileReader() {
+	file.close();
+
+	delete [] listCellType;
+	delete [] listCellSimbol;
+	delete [] listCageSimbol;
+	delete [] listCageType;
+	delete [] listNAnimal;
+
+	for(int i=0; i<nbrs; i++) {
+		delete [] maps[i];
+	}
+	delete [] maps;
+
+	for(int i=0; i<GetSumCageArea(); i++) {
+		delete [] listPos[i];
+	}
+	delete [] listPos;
+
+	delete [] listNCageArea;
+}
+/**
+* @brief Membaca seluruh file 
+*/
+void FileReader::Read() {
+	file >> nbrs >> nkol;
+	
+	file >> nCellType;
+	listCellType = new string[nCellType];
+	listCellSimbol = new char[nCellType];
+	for(int i=0; i<nCellType; i++) {
+		file >> listCellSimbol[i] >> listCellType[i];
+	}
+
+	maps = new char*[nbrs];
+	for(int i=0; i<nbrs; i++) {
+		maps[i] = new char[nkol];
+	}
+
+	for(int i=0; i<nbrs; i++) {
+		for(int j=0; j<nkol; j++) {
+			file >> maps[i][j];
+		}
+	}
+
+	file >> nCage;
+	listCageSimbol = new char[nCage];
+	listCageType = new string[nCage];
+	listNCageArea = new int[nCage];
+	for(int i=0; i<nCage; i++){
+		file >> listCageSimbol[i];
+		file >> listCageType[i];
+		file >> listNCageArea[i];
+	}
+
+	listNAnimal = new int[nCage];
+	listPos = new int*[GetSumCageArea()];
+	for(int i=0; i<GetSumCageArea(); i++) {
+		listPos[i] = new int [2];
+	}
+
+	int c=0;
+	int d=0;
+	for(int i=0; i<nCage; i++) {
+		for(int j=0; j<listNCageArea[i]; j++) {
+			file >> listPos[c][0] >> listPos[c][1];
+			c++;
+		}
+		file >> listNAnimal[d];
+		d++;
+	}
+}
+
+int FileReader::GetSumCageArea(){
+	int sum=0;
+	for(int i=0; i<nCage; i++){
+		sum = sum + listNCageArea[i];	
+	}
+
+	return sum;
+}
+
+/**
+* @brief Getter. Mendapatkan nilai baris area Zoo
+* @return nbrs
+*/
+int FileReader::GetNBrs() {
+	return nbrs;
+}
+/**
+* @brief Getter. Mendapatkan nilai kolom area Zoo
+* @return nkol
+*/
+int FileReader::GetNKol() {
+	return nkol;
+}
+/**
+* @brief Getter. Mendapatkan jumlah tipe Cell yang ada
+* @return nCellType
+*/
+int FileReader::GetNCellType() {
+	return nCellType;
+}
+/**
+* @brief Getter. Mendapatkan jumlah Cage yang akan dibuat
+* @return nCage
+*/
+int FileReader::GetNCage() {
+	return nCage;
+}
+
+/**
+* @brief Getter. Mendapatkan tipe Cell ke i
+* @param i Index tipe Cell yang akan dikembalikan
+* @return listCellType[i]
+*/
+string FileReader::GetCellType(int i) {
+	return listCellType[i];
+}
+/**
+* @brief Getter. Mendapatkan simbol Cell ke i
+* @param i Index simbol Cell yang akan dikembalikan
+* @return listCellSimbol[i]
+*/
+char FileReader::GetCellSimbol(int i) {
+	return listCellSimbol[i];
+}
+/**
+* @brief Getter. Mendapatkan simbol Cell pada maps[i][j]
+* @param i Index baris maps
+* @param j Indeks kolom maps
+* @return maps[i][j]
+*/
+char FileReader::GetMaps(int i, int j) {
+	return maps[i][j];
+}
+/**
+* @brief Getter. Mendapatkan tipe Cage ke i
+* @param i Index tipe Cage yang akan dikembalikan
+* @return listCageType[i]
+*/
+string FileReader::GetCageType(int i) {
+	return listCageType[i];
+}
+/**
+* @brief Getter. Mendapatkan simbol Cage ke i
+* @param i Index simbol Cage yang akan dikembalikan
+* @return listCageSimbol[i]
+*/
+char FileReader::GetCageSimbol(int i) {
+	return listCageSimbol[i];
+}
+/**
+* @brief Getter. Mendapatkan area Cage ke i
+* @param i Index area Cage yang akan dikembalikan
+* @return listNCageArea[i]
+*/
+int FileReader::GetNCageArea(int i) {
+	return listNCageArea[i];
+}
+/**
+* @brief Getter. Mendapatkan jumlah Animal ke i
+* @param i Index jumlah Animal yang akan dikembalikan
+* @return listNAnimal[i]
+*/
+int FileReader::GetNAnimal(int i) {
+	return listNAnimal[i];
+}
+/**
+* @brief Getter. Mendapatkan posisi X Cell Cage ke i
+* @param i Index posisi X Cell Cage yang akan dikembalikan
+* @return listPos[i][0]
+*/
+int FileReader::GetPosX(int i) {
+	return listPos[i][0];
+}
+/**
+* @brief Getter. Mendapatkan posisi Y Cell Cage ke i
+* @param i Index posisi Y Cell Cage yang akan dikembalikan
+* @return listPos[i][1]
+*/
+int FileReader::GetPosY(int i) {
+	return listPos[i][1];
+}
---
+++ VZ03/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "virtual_zoo.h"
+
+using namespace std;
+int main(){
+
+	VirtualZoo* virtual_zoo = new VirtualZoo("input.txt");
+
+	int n;
+	do {
+		cout << "1. Display Virtual Zoo All" << endl;
+		cout << "2. Display Virtual Zoo " << endl;
+		cout << "3. Tour Virtual Zoo" << endl;
+		cout << "4. Count Total Animal Feeds" <<endl;
+		cout << "0. Exit" << endl;
+		cin >> n;
+		if (n == 1) {
+			virtual_zoo->PrintVirtualZoo();
+		} else if (n == 2) {
+			int kiri,atas,kanan,bawah;
+
+			cout << "Batas kiri: "; cin >> kiri;
+			cout << "Batas atas: "; cin >> atas;
+			cout << "Batas kanan: "; cin >> kanan;
+			cout << "Batas bawah: "; cin >> bawah;
+			if (virtual_zoo->IsInRage(kiri,atas,kanan,bawah)) {
+				virtual_zoo->PrintVirtualZoo(kiri,atas,kanan,bawah);
+			} else {
+				cout << "Out of rage " << endl;
+			}
+			
+		} else if (n == 3) {
+			virtual_zoo->Tour();
+		} else if (n == 4) {
+			cout <<"Total makanan : "<< virtual_zoo->GetTotalMakanan() << endl;
+		}
+
+	} while (n!=0);
+	
+	return 0;
+}
+
